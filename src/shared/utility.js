81c88180af7d240c89e48fbb67240227c5fc0380
Yanhong95 <yanhongmain@gmail.com>
2020-09-30 17:16:01 -0400

    tried to fix request error

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
     baseURL: process.env.NODE_ENV === "development" ? 
-    process.env.REACT_APP_DEV_SERVICE_URL : process.env.REACT_APP_PRO_SERVICE_URL
+    'https://ink-server.us-east-2.elasticbeanstalk.com' : 'http://localhost:8080'
+    // process.env.REACT_APP_DEV_SERVICE_URL : process.env.REACT_APP_PRO_SERVICE_URL
 });
 
 export const updateObject = (oldObject, updatedProperties) => {
