6b1339b536cc994d9c35f550c5bc47ccd28f6fa7
Yanhong95 <yanhongmain@gmail.com>
2020-09-30 17:23:39 -0400

    tried to fix request error

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
     baseURL: process.env.NODE_ENV === "development" ? 
-    'https://ink-server.us-east-2.elasticbeanstalk.com' : 'http://localhost:8080'
+    'http://localhost:8080' : 'https://ink-server.us-east-2.elasticbeanstalk.com' 
     // process.env.REACT_APP_DEV_SERVICE_URL : process.env.REACT_APP_PRO_SERVICE_URL
 });
 
